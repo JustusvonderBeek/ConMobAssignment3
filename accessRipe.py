076a388a450105391dfd6fa3bf14754b47333afa
Justus von der Beek <beek@in.tum.de>
2023-02-03 18:55:55 +0100

    Added filtering tags

--- accessRipe.py
+++ accessRipe.py
@@ -33,6 +33,27 @@ def filterWifiNodes():
     with open("probes.json", "w") as file:
         json.dump(response.json(), file)
 
+def filterLocalNodes(input, tags):
+    """
+    Gets the path to the json file containing all nodes. Extracting the nodes matching the list of tags.
+    """
+
+    
+    with open(input, "r") as file:
+        nodes = json.load(file)
+        # print(nodes[:1])
+        filtered = list()
+        for elem in nodes["objects"]:
+            if elem["status_name"] == "Connected":
+                for tag in tags:
+                    if tag in elem["tags"]:
+                        filtered.append(elem["id"])
+
+            # nodes = nodes[nodes[:]["status_name"] == "Connected"]
+        
+        print(filtered)
+
+
 if __name__ == '__main__':
 
     parser = ArgumentParser(description='Generate performance charts for throughput values from pcap file')
@@ -40,4 +61,5 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', type=str, required=True)
     
 
-    filterWifiNodes()
+    # filterWifiNodes()
+    filterLocalNodes("20230202.json", ["wi-fi", "wireless", "wifi", "WiFi", "WIFI", "wireless-isp"])
