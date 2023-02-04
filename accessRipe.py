8c06a2585d1c9c45d6e16dc9d8244b5c67273f01
Justus <beek@in.tum.de>
2023-02-04 12:36:58 +0100

    Added method to filter for connected

--- accessRipe.py
+++ accessRipe.py
@@ -3,6 +3,7 @@ import json
 import posixpath
 from argparse import ArgumentParser
 from urllib.parse import urljoin
+from collections import defaultdict
 
 base_url = "https://atlas.ripe.net"
 
@@ -33,33 +34,47 @@ def filterWifiNodes():
     with open("probes.json", "w") as file:
         json.dump(response.json(), file)
 
-def filterLocalNodes(input, tags):
+def filterLocalNodes(input, output, field, tags):
     """
     Gets the path to the json file containing all nodes. Extracting the nodes matching the list of tags.
     """
-
     
+    filtered_dict = defaultdict(list)
     with open(input, "r") as file:
         nodes = json.load(file)
-        # print(nodes[:1])
-        filtered = list()
         for elem in nodes["objects"]:
-            if elem["status_name"] == "Connected":
-                for tag in tags:
-                    if tag in elem["tags"]:
-                        filtered.append(elem["id"])
+            for tag in tags:
+                if elem[field] == tag:
+                    filtered_dict["objects"].append(elem)
+                    # for tag in tags:
+                    #     if tag in elem["tags"]:
+                    #         filtered.append(elem["id"])
 
-            # nodes = nodes[nodes[:]["status_name"] == "Connected"]
-        
-        print(filtered)
+    # with open(output, "w") as output_file:
+        # json.dump(filtered_dict, output_file, indent=2)
+
+    return filtered_dict
+
+def filterConnected(args):
+    """
+    Filtering all nodes for the connected ones and writing these to a special file.
+    """
+
+    filtered = filterLocalNodes(args.input, "connected.json", "status_name", ["Connected", "connected"])
+
+    with open(args.output, "w") as output_file:
+        json.dump(filtered, output_file)
+        print(f"Wrote filtered connected nodes to '{args.output}'")
 
 
 if __name__ == '__main__':
 
     parser = ArgumentParser(description='Generate performance charts for throughput values from pcap file')
-    parser.add_argument('-i', '--input', action="append", default=[], required=True)
-    parser.add_argument('-o', '--output', type=str, required=True)
-    
+    parser.add_argument('-i', '--input', type=str, default="20230202.json")
+    parser.add_argument('-o', '--output', type=str, default="connected.json")
+    args = parser.parse_args()
 
     # filterWifiNodes()
-    filterLocalNodes("20230202.json", ["wi-fi", "wireless", "wifi", "WiFi", "WIFI", "wireless-isp"])
+    # filterLocalNodes("20230202.json", "connected.json", ["wi-fi", "wireless", "wifi", "WiFi", "WIFI", "wireless-isp"])
+
+    filterConnected(args)
